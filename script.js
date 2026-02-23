47537caf2ffeff3867d7a208e3da6f001eb16169
lynjung <snowinaugust4@gmail.com>
2026-02-23 18:58:36 -0500

    js add

---
+++ script.js
@@ -0,0 +1,8 @@
+const title = document.querySelector(".logo");
+title.innerHTML = "Flowie ✨";
+
+const firstCard = document.querySelector(".card");
+firstCard.classList.add("highlight");
+
+const button = document.querySelector("button");
+button.style.backgroundColor = "red";
