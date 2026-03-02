135c5e34aa81970a1a7865eab9bbfe6138fa91f1
lynjung <snowinaugust4@gmail.com>
2026-03-01 22:51:54 -0500

    js change

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const title = document.querySelector(".logo");
-title.innerHTML = "Flowie ✨";
+const logo = document.querySelector(".logo");
+const menuIcon = document.querySelector(".menu-icon");
 
+const firstButton = document.querySelector(".button");
 const firstCard = document.querySelector(".card");
-firstCard.classList.add("highlight");
 
-const button = document.querySelector("button");
-button.style.backgroundColor = "red";
+menuIcon.addEventListener("click", function() {
+    logo.innerHTML = "Study🎧";
+});
+
+firstButton.addEventListener("mouseover", function() {
+    firstCard.style.backgroundColor = "lightpink";
+});
+
+firstButton.addEventListener("mouseout", function() {
+    firstCard.style.backgroundColor = "lightyellow";
+});
