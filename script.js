7ee084897a4a73f780ffaf90f3204cdce7aef31e
lynjung <snowinaugust4@gmail.com>
2026-04-15 13:08:28 -0400

    js implementations added

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 const logo = document.querySelector(".logo");
 const menuIcon = document.querySelector(".menu-icon");
+const nav = document.querySelector(".nav-desktop");
 
-const firstButton = document.querySelector(".button");
-const firstCard = document.querySelector(".card");
+const firstButton = document.querySelector("#button1");
+const firstCard = document.querySelector("#card1");
+
+const secondButton = document.querySelector("#button2");
+const secondText = document.querySelector("#text2");
+
+const thirdButton = document.querySelector("#button3");
+const thirdText = document.querySelector("#text3");
 
 menuIcon.addEventListener("click", function() {
-    logo.innerHTML = "Study🎧";
+    logo.innerHTML = "Study Mode🎧";
+
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    }
+    else {
+        nav.style.display = "block";
+    }
 });
 
 firstButton.addEventListener("mouseover", function() {
-    firstCard.style.backgroundColor = "lightpink";
+    firstCard.style.backgroundColor = "lightgreen";
+    firstCard.style.transform = "scale(1.03)";
 });
 
 firstButton.addEventListener("mouseout", function() {
     firstCard.style.backgroundColor = "lightyellow";
+    firstCard.style.transform = "scale(1)";
+});
+
+secondButton.addEventListener("click", function() {
+    secondText.innerHTML = "Task Planning Mode 🗒️";
+});
+
+thirdButton.addEventListener("click", function() {
+    thirdText.innerHTML = "Currently viewing insights...";
 });
